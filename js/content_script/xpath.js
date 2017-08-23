ad765e13f828dd12497076af920a612da983e63e
Stephen Shodimu <technicalflaw@users.noreply.github.com>
2017-08-23 18:37:33 +0100

    Further optimize XPath getter

--- js/content_script/xpath.js
+++ js/content_script/xpath.js
@@ -25,45 +25,22 @@ var _xpath = {
      * @return {string} xpath of node, or empty string if no tests could be identified
      */
     _getPath: (node) => {
-        let nodeTests = []
+        let tests = []
 
         // if the chain contains a non-(element|text) node type, we can go no further
-        for (; node && (node.nodeType === Node.ELEMENT_NODE || node.nodeType === Node.TEXT_NODE); node = node.parentNode) {
-            // if the node is an element with a specific, unique id, it can become the root of the path,
-            // and since we're going from node to document root, we have all we need.
-            if (node.nodeType === Node.ELEMENT_NODE &&
-                node.id.length > 0 &&
-                node.ownerDocument.querySelectorAll(`#${node.id}`).length === 1) {
-                // because the first item of the path array is prefixed with '/', this will become 
-                // a double slash (select all elements). But as there's only one result, we can use [1]
-                // eg: //*[@id='something'][1]/div/text()
-                nodeTests.unshift(`/*[@id="${node.id}"][1]`)
-                break
-            }
-
-            // Get node index by counting previous siblings of the same name & type
-            let index = 1
-
-            for (let siblingNode = node.previousSibling; siblingNode; siblingNode = siblingNode.previousSibling) {
-                // Skip DTD,
-                // Skip nodes of differing type AND name (tagName for elements, #text for text),
-                // as they are indexed by node type
-                if (siblingNode.nodeType === Node.DOCUMENT_TYPE_NODE ||
-                    node.nodeType !== siblingNode.nodeType ||
-                    siblingNode.nodeName !== node.nodeName) {
-                    continue
-                }
-
-                index++
-            }
+        for (;
+            node && (node.nodeType === Node.ELEMENT_NODE || node.nodeType === Node.TEXT_NODE);
+            node = node.parentNode) {
+            // node test predicates
+            let predicates = []
 
             // format node test for current node
-            let nodeTest = (() => {
+            let test = (() => {
                 switch (node.nodeType) {
                     case Node.ELEMENT_NODE:
                         // naturally uppercase. I forget why I force it lower.
                         return node.nodeName.toLowerCase()
-                
+
                     case Node.TEXT_NODE:
                         return 'text()'
 
@@ -72,16 +49,55 @@ var _xpath = {
                 }
             })()
 
-            // nodes at index 1 (1-based) are implicitly selected
-            if (index > 1) {
-                nodeTest += `[${index}]`
+            if (node.nodeType === Node.ELEMENT_NODE && node.id.length > 0) {
+                // if the node is an element with a unique id within the *document*, it can become the root of the path,
+                // and since we're going from node to document root, we have all we need.
+                if (node.ownerDocument.querySelectorAll(`#${node.id}`).length === 1) {
+                    // because the first item of the path array is prefixed with '/', this will become 
+                    // a double slash (select all elements). But as there's only one result, we can use [1]
+                    // eg: //span[@id='something']/div[3]/text()
+                    tests.unshift(`/${test}[@id="${node.id}"]`)
+                    break
+                } 
+                
+                if (node.parentElement && !Array.prototype.slice
+                    .call(node.parentElement.children)
+                    .some(sibling => sibling !== node && sibling.id === node.id)) {
+                    // There are multiple nodes with the same id, but if the node is an element with a unique id 
+                    // in the context of its parent element we can use the id for the node test
+                    predicates.push(`@id="${node.id}"`)
+                }
             }
 
-            nodeTests.unshift(nodeTest)
+            if (predicates.length === 0) {
+                // Get node index by counting previous siblings of the same name & type
+                let index = 1
+
+                for (let sibling = node.previousSibling; sibling; sibling = sibling.previousSibling) {
+                    // Skip DTD,
+                    // Skip nodes of differing type AND name (tagName for elements, #text for text),
+                    // as they are indexed by node type
+                    if (sibling.nodeType === Node.DOCUMENT_TYPE_NODE ||
+                        node.nodeType !== sibling.nodeType ||
+                        sibling.nodeName !== node.nodeName) {
+                        continue
+                    }
+
+                    index++
+                }
+
+                // nodes at index 1 (1-based) are implicitly selected
+                if (index > 1) {
+                    predicates.push(`${index}`)
+                }
+            }
+
+            // format predicates
+            tests.unshift(test + predicates.map(p => `[${p}]`).join(''))
         } // end for
 
         // return empty path string if unable to create path
-        return nodeTests.length === 0 ? "" : `/${nodeTests.join('/')}`
+        return tests.length === 0 ? "" : `/${tests.join('/')}`
     },
 
     /**
